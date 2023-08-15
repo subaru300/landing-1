6da90f8a6849af519d4d02b12040da4eda11fa9c
Sergey Cherep <135381699+subaru300@users.noreply.github.com>
2023-08-15 12:51:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.materials__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+window.addEventListener('scroll', function () {
+  scrollY > 0
+    ? document.querySelector('.header').classList.add('scroll')
+    : document.querySelector('.header').classList.remove('scroll');
+});
